72c04ccd72779c3096c7434992a1fba0a5fa989a
Suany Chalan <suany_0805@hotmail.com>
2023-01-20 15:40:18 -0500

    Chapter1 exercises

---
+++ Chapter1.hs
@@ -0,0 +1,32 @@
+
+{- 
+double (double 2)
+double(2)+ double(2)
+(2+2)   +  (2+2) 
+4+4
+8 -}
+
+
+{- sum[x]=x
+sum[2] -}
+
+myproduct []=1
+myproduct (n:ns) = n * myproduct ns
+-- product [2,3,4]
+
+-- 3) moving qsort larger to the beginning and qsort smaller to the end 
+
+qsort1:: Ord a => [a] -> [a]
+qsort1 [] = []
+qsort1 (x:xs) = qsort1 large ++[x] ++ qsort1 small
+               where 
+                  large = [b | b <- xs , b > x] 
+                  small = [a | a <- xs , a <= x]
+
+-- 4) duplicates will be deleted
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+               where 
+                  smaller = [a| a <- xs , a <x]
+                  larger = [b | b <- xs , b>x]
