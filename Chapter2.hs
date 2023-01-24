16c83ef578cd460be755ee18e27a54334d0b51d6
Suany Chalan <suany_0805@hotmail.com>
2023-01-24 11:06:53 -0500

    Completed exercises chapter 2

--- Chapter2.hs
+++ Chapter2.hs
@@ -1,4 +1,4 @@
-{- 
+{-
 --Gets the 1st element of a non-empty list
 head [2,4,6]
 2
@@ -6,7 +6,7 @@ head [2,4,6]
 --Removes the 1st elemnt of a non-empty list
 tail [1,2,3,4,5]
 [2,3,4,5]
---select the nth element of a list  
+--select the nth element of a list
 [3,5,7,9] !!1
 5
 --Select the x first elements from a list
@@ -37,20 +37,61 @@ product[1,2,3,4,5,6,7,8,9]
 reverse [2,4,6,8]
 [8,6,4,2] -}
 
-
-double :: Num a=> a->a
+double :: Num a => a -> a
 double x = x + x
 
-quadruple :: Num a=> a->a
-quadruple x = double(double x)
-{- 
+quadruple :: Num a => a -> a
+quadruple x = double (double x)
+
+{-
 quadruple 10
 40
  -}
- -- 
+--
 
-factorial:: (Num a, Enum a )=>a->a
-factorial n = product [1..n]
+factorial :: (Num a, Enum a) => a -> a
+factorial n = product [1 .. n]
 
 average :: Foldable t => t Int -> Int
-average (ns)= sum ns `div` length ns
+average ns = sum ns `div` length ns
+
+-- 2) parenthesise the following numeric expressions
+
+{-
+> 2 ^ 3 *4
+( 2 ^ 3) *4
+
+> 2*3+4*5
+(2*3)+(4*5)
+
+> 2 + 3 * 4 ^ 5
+2 +  (3 *(4 ^ 5))
+
+-}
+
+-- 3) Fix the syntactic errors
+{-  N = a 'div' length xs
+     where
+        a=10
+       xs=[1,2,3,4,5]
+ -}
+
+n = a `div` length xs
+  where
+    a = 10
+    xs = [1, 2, 3, 4, 5]
+
+-- 4) Show how the function 'last' could be defined in terms of the library introduced in this chapter
+
+myLast :: [a] -> a
+myLast [] = error "Empty list"
+myLast xs = head (reverse xs)
+
+-- 5)Show how 'init'could be defined in two different ways
+myInit1 :: [a] -> [a]
+myInit1 [] = error "Empty list"
+myInit1 xs = reverse (drop 1 (reverse xs))
+
+myInit2 :: [a] -> [a]
+myInit2 [] = error "Empty list"
+myInit2 xs = take (length xs - 1) xs
